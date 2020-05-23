3056f14c989148a79f931640e909612d00ce23bd
Robert Davies <rob.davies1991@gmail.com>
2020-05-23 19:36:54 +0100

    fixed main char when logged out

--- characters/contexts.py
+++ characters/contexts.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404
 from .models import Character
 from django.contrib.auth.models import User
-
+from django.shortcuts import get_object_or_404
 
 def main_character(request):
-    user = User.objects.get(id=request.user.id)
-    selected_main_character = Character.objects.filter(user_id=user.id, banned='No', main_character='Yes').first()
-
-    return { 'selected_main_character':selected_main_character }
+    if request.user.is_authenticated:
+        user = User.objects.get(id=request.user.id)
+        selected_main_character = Character.objects.filter(user_id=user.id, banned='No', main_character='Yes').first()
+        return { 'selected_main_character':selected_main_character }
+    else:
+        return ''
